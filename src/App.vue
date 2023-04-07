a4a57d9d6c1c7341e504094a1a1a8179c06bd028
Gabriel Ramirez <geramirezl@unal.edu.co>
2023-04-06 21:26:51 -0500

    first changes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <UserSkills />
+  </div>
+</template>
+
+<script>
+import UserSkills from './components/UserSkills.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UserSkills,
+  },
+};
+</script>
+
---
+++ src/components/UserSkills.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <h1>{{ username }}</h1>
+      <ul>
+        <li v-for="skill in skills" :key="skill.id">
+          <b>{{ skill.name }}:</b> {{ skill.proficiency }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: 'gaenrale',
+        skills: [],
+      };
+    },
+    mounted() {
+      axios.get('https://cors-anywhere.herokuapp.com/https://torre.bio/api/bios/gaenrale', {
+        headers: {
+          'x-requested-with': 'XMLHttpRequest',
+        },
+      })
+        .then(response => {
+          this.skills = response.data.strengths;
+          console.log(this.skills)
+          console.log("exist?")
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  };
+  </script>
